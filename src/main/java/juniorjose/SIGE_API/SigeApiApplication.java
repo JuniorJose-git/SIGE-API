506ceca606793bee945ab7c943090e9a2198f13e
JuniorJose-git <joseronaldosantus@gmail.com>
2025-11-22 19:14:51 -0300

    ➕ adicionado dependências do springboot

---
+++ src/main/java/juniorjose/SIGE_API/SigeApiApplication.java
@@ -0,0 +1,13 @@
+package juniorjose.SIGE_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/juniorjose/SIGE_API/SigeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package juniorjose.SIGE_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
